1d7c729eed8985cf03cab2c57ba4fe547bd7ff0c
Mr- 0 <oldcrocs21@gmail.com>
2024-08-24 16:20:22 +0000

    X00TD: Afterlify
    Signed-off-by: Mr- 0 <oldcrocs21@gmail.com>

--- afterlife_X00TD.mk
+++ afterlife_X00TD.mk
@@ -9,23 +9,24 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Blaze stuff
-$(call inherit-product, vendor/blaze/config/common_full_phone.mk)
+$(call inherit-product, vendor/afterlife/config/common_full_phone.mk)
 
-# Blaze official flags
+# afterlife official flags
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_FACE_UNLOCK_SUPPORTED := true
-BLAZE_BUILD_TYPE := OFFICIAL
-BLAZE_MAINTAINER := fakedotkit
+AFTERLIFE_MAINTAINER := Romeo
 
-# GApps flags
-TARGET_GAPPS_ARCH := arm64
-WITH_GAPPS := true
+# Offline Charging
+USE_PIXEL_CHARGING := true
+
+# disable/enable blur support, default is false
+TARGET_SUPPORTS_BLUR := true
 
 # Inherit from X00TD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := blaze_X00TD
+PRODUCT_NAME := afterlife_X00TD
 PRODUCT_DEVICE := X00TD
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := Zenfone Max Pro M1
@@ -40,3 +41,9 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 # Build fingerprint
 BUILD_FINGERPRINT := "google/shiba/shiba:14/UQ1A.240205.004/11269751:user/release-keys"
+
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.vendor.build.fingerprint=$(BUILD_FINGERPRINT) \
+    ro.system.build.fingerprint=$(BUILD_FINGERPRINT) \
+    ro.product.build.fingerprint=$(BUILD_FINGERPRINT) \
+    ro.system_ext.build.fingerprint=$(BUILD_FINGERPRINT)
--- device.mk
+++ device.mk
@@ -304,7 +304,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-blaze
+    $(LOCAL_PATH)/overlay-afterlife
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
